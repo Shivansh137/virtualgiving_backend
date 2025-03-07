adc1a9c0a8996daf78e4b2e64825f70bdb6534a0
Shivansh Agrawal <shivanshagrawal137@gmail.com>
2025-03-07 22:35:18 +0530

    Initialized using Spring Initializer

---
+++ src/main/java/com/virtualgiving/VirtualgivingApplication.java
@@ -0,0 +1,13 @@
+package com.virtualgiving;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualgivingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualgivingApplication.class, args);
+	}
+
+}
